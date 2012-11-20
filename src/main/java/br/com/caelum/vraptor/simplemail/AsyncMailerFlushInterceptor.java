3ce14ae30685a875b3f71d3068c288b86ee10e54
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2012-11-20 16:23:04 -0200

    Convenience interceptor that sends all postponed emails before ending a request

---
+++ src/main/java/br/com/caelum/vraptor/simplemail/AsyncMailerFlushInterceptor.java
@@ -0,0 +1,39 @@
+package br.com.caelum.vraptor.simplemail;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.caelum.vraptor.InterceptionException;
+import br.com.caelum.vraptor.Intercepts;
+import br.com.caelum.vraptor.core.InterceptorStack;
+import br.com.caelum.vraptor.interceptor.ExecuteMethodInterceptor;
+import br.com.caelum.vraptor.interceptor.Interceptor;
+import br.com.caelum.vraptor.resource.ResourceMethod;
+
+@Intercepts(before = ExecuteMethodInterceptor.class)
+public class AsyncMailerFlushInterceptor implements Interceptor {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AsyncMailerFlushInterceptor.class);
+
+	private final AsyncMailer mailer;
+
+	public AsyncMailerFlushInterceptor(AsyncMailer mailer) {
+		this.mailer = mailer;
+	}
+
+	@Override
+	public boolean accepts(ResourceMethod method) {
+		return true;
+	}
+
+	@Override
+	public void intercept(InterceptorStack stack, ResourceMethod method, Object controller) throws InterceptionException {
+		try {
+			stack.next(method, controller);
+			mailer.deliverPostponedMails();
+		} catch (Exception e) {
+			LOGGER.error("The following emails were not delivered because of an exception: {}", mailer.clearPostponedMails());
+			throw new InterceptionException(e);
+		}
+	}
+}
